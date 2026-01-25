97abc96a2fd423d8499678793199e8309b7f441c
brandonlau6 <brandonctx0@gmail.com>
2026-01-25 17:33:55 -0500

    feat: storage backend

--- storageBackend/configuration.nix
+++ storageBackend/configuration.nix
@@ -44,19 +44,19 @@
 
   networking.hostName = "storageBackend";
 
-  #networking = {
-  #  interfaces = {
-  #    ens18.ipv4.addresses = [{
-  #      address = "192.168.1.180";
-  #      prefixLength = 24;
-  #    }];
-  #
-  #  };
-  #  defaultGateway = {
-  #      address = "192.168.1.1";
-  #      interface = "ens18";
-  #  };
-  #};
+  networking = {
+    interfaces = {
+      ens18.ipv4.addresses = [{
+        address = "192.168.1.160";
+        prefixLength = 24;
+      }];
+  
+    };
+    defaultGateway = {
+        address = "192.168.1.1";
+        interface = "ens18";
+    };
+  };
 
 
   networking.networkmanager.dhcp = "dhcpcd";
