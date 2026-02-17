0dd8e73eb51bc9a944b5e5ce31b03285f0723ba3
brandonlau6 <brandonctx0@gmail.com>
2026-02-17 11:45:02 -0500

    added plugins

--- htpc/configuration.nix
+++ htpc/configuration.nix
@@ -63,7 +63,7 @@
   services.xserver.desktopManager.kodi.enable = true;
   services.displayManager.autoLogin.user = "kodi";
   services.xserver.displayManager.lightdm.greeter.enable = false;
-  services.xserver.desktopManager.kodi.package = pkgs.kodi.withPackages (pkgs: with pkgs; [ jellyfin ]);
+  services.xserver.desktopManager.kodi.package = pkgs.kodi.withPackages (pkgs: with pkgs; [ jellyfin youtube netflix invidious ]);
 
   # Define a user account
   users.extraUsers.kodi.isNormalUser = true;
