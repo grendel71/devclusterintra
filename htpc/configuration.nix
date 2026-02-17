0c90316e734edf2c79910a3827df54ab1a9662c7
brandonlau6 <brandonctx0@gmail.com>
2026-02-17 01:28:48 -0500

    pipewire

--- htpc/configuration.nix
+++ htpc/configuration.nix
@@ -16,6 +16,14 @@
     efiSupport = true;
     efiInstallAsRemovable = true;
   };
+  services.pipewire = {
+  enable = true;
+  alsa.enable = true;
+  alsa.support32Bit = true;
+  pulse.enable = true;
+  # If you want to use JACK applications, uncomment this
+  #jack.enable = true;
+  };
   services.openssh = {
     enable = true;
     openFirewall = true;
